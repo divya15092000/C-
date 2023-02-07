ce47421b0e66dc28813f6aa15b4584d78a10419c
Divya Saini <124121090+divya15092000@users.noreply.github.com>
2023-02-07 10:42:16 +0530

    Print triangle in C#

---
+++ triangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trianglee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            a = 50;
+            for (b = 1; b <= a; b++)
+            {
+                for(c = 1; c <= a - b; c++)
+                {
+                    Console.Write(" ");
+                }
+                for (d = 1; d <= b; d++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+              
+            }
+        }
+    }
+}
