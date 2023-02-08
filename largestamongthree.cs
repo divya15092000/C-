3959939e2284c4301e98dda4e0679ce6682ba71a
Divya Saini <124121090+divya15092000@users.noreply.github.com>
2023-02-08 11:00:19 +0530

    largestamongthree

---
+++ largestamongthree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rightriangle
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter number:");
+           int a = int.Parse(Console.ReadLine());
+          Console.WriteLine("number is: " + a);
+            Console.WriteLine("Enter number:");
+            int b = int.Parse(Console.ReadLine());
+           Console.WriteLine("number is: " + b);
+            Console.WriteLine("Enter number:");
+            int c = int.Parse(Console.ReadLine());
+          Console.WriteLine("number is: " + c);
+            if (a>b&&a>c)
+            {
+                Console.WriteLine("a is greater");
+            }else if(b>a && b > a)
+            {
+                Console.WriteLine("b is greater");
+            }
+            else
+            {
+                Console.WriteLine("c is greater");
+            }
+        }
+    }
+}
