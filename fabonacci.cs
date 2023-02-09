891a8b8f0ac67d26b6f35591a3fb219502fac1c8
Divya Saini <124121090+divya15092000@users.noreply.github.com>
2023-02-09 10:46:08 +0530

    fibonacci series in C#

---
+++ fabonacci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fabo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3, a = 10, i;
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            for (i = 2; i < a; i++)
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(n3);
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
