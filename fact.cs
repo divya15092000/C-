38827cc491747c3406779066af61758ace442d28
Divya Saini <124121090+divya15092000@users.noreply.github.com>
2023-02-13 10:22:08 +0530

    factorial of a number

---
+++ fact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fact
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1, i;
+            Console.WriteLine("Enter the number: ");
+            int a = int.Parse(Console.ReadLine());
+            for (i = 1; i <= a; i++)
+            {
+                n = n * i;
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
